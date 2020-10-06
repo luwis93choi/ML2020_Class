f6458ac53355cbd535a8c919b49067a21dad2ebe
luwis93choi <luwis93choi@hotmail.com>
2020-10-06 23:56:02 +0900

    Editted comments

--- Assignment_04_Classification/KOR_ver/03_Breast_Cancer_LogisticRegression_KNN_GaussianNB_RandomForest_DecisionTree_SVM (KOR).py
+++ Assignment_04_Classification/KOR_ver/03_Breast_Cancer_LogisticRegression_KNN_GaussianNB_RandomForest_DecisionTree_SVM (KOR).py
@@ -2,6 +2,7 @@
 ### Q3. Breast Cancer Classification using Logistic Regression, KNN, ###
 ###     Gaussian Naive Bayes, Random Forest, Decision Tree, and SVM  ###
 ########################################################################
+
 import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
@@ -15,84 +16,101 @@ from sklearn import svm
 from sklearn import metrics
 from matplotlib import pyplot as plt
 
-breast_cancer_dataset = load_breast_cancer()
+### 데이터셋 준비 및 전처리 ###
+breast_cancer_dataset = load_breast_cancer()	# 유방암 데이터셋 준비
 
-print('Breast Cancer Dataset Key : ', breast_cancer_dataset.keys())
+print('Breast Cancer Dataset Key : ', breast_cancer_dataset.keys())		# 유방암 데이터셋의 Attribute (구성요소) 출력
 
-print('Breast Cancer Dataset Target : ', breast_cancer_dataset.target_names)
+print('Breast Cancer Dataset Target : ', breast_cancer_dataset.target_names)	# 유방암 데이터셋의 Target/Label에 대한 내용 출력 (유방암 악성 여부)
 
-breast_cancer_data = pd.DataFrame(breast_cancer_dataset.data)
-breast_cancer_label = pd.DataFrame(breast_cancer_dataset.target)
+breast_cancer_data = pd.DataFrame(breast_cancer_dataset.data)			# 유방암 데이터셋의 Feature 데이터를 Pandas DataFrame 형태로 전환
+breast_cancer_label = pd.DataFrame(breast_cancer_dataset.target)		# 유방암 데이터셋의 Target/Label을 Pandas DataFrame 형태로 전환
 
-print(breast_cancer_data.head())
+print(breast_cancer_data.head())	# 유방암 데이터셋의 Feature 데이터 최초 5개를 출력함
+					# 유방암 데이터셋의 30개의 Feature로 구성되어있다는 것을 알 수 있음
 
-print(breast_cancer_label.head())
+print(breast_cancer_label.head())	# 유방암 데이터셋의 Target/Label 데이터 최초 5개를 출력함
 
-print(breast_cancer_data.describe())
+print(breast_cancer_data.describe())	# 유방암 데이터셋의 Feature 데이터에 대한 통계학적 정보를 출력함
 
+### 데이터셋 Training/Test 분리 ###
+# 데이터셋 데이터를 Training 70% : Test 30%로 분리함
 train_data, test_data, train_label, test_label = train_test_split(breast_cancer_data, breast_cancer_label, test_size=0.3, random_state=23)
 
-# Dataset Standardization 
-scaler = StandardScaler()   # Standardize the dataset using mean and variance, and convert it into z-score values
-train_data = scaler.fit_transform(train_data)
-test_data = scaler.transform(test_data)
+### 데이터셋 표준화 (Standardization) ###
+# Feature 데이터에 대해서 Mean과 Standard Deviation를 이용한 z-score로 표준화를 수행함
+# 각 Feature마다 다른 단위 체계를 사용하기 때문에 Scaling에 의해 데이터의 분포가 단위별로 다르게 나타나는 현상이 발생함
+# Mean과 Standard Deviation을 통해 각 Feature에 해당되는 데이터 모음의 분포를 Mean 0, Standard Deviation 1으로 통일시킬 수 있음
+# 각 Feature의 데이터 모음을 동일 분포로 통일시킴으로서 Scaling에 의한 부정적인 효과를 최소화함
+scaler = StandardScaler()			# 데이터셋을 z-score로 전환할 수 있는 연산기 준비
+train_data = scaler.fit_transform(train_data)	# Train 데이터에 대해 z-score로 표준화함
+test_data = scaler.transform(test_data)		# Test 데이터에 대해 z-score로 표준화함
 
-### Logistic Regression ###
-LogR_model = LogisticRegression()
-LogR_model.fit(train_data, train_label)
+### 유방암 데이터셋에 대해 Logistic Regression 이용한 Classification 적용 ###
+LogR_model = LogisticRegression()		# Logistic Regression Classifier 준비
+LogR_model.fit(train_data, train_label)		# Train 데이터에 대해 Logistic Regression Fitting 수행
 
-print('Logistic Regression Train Score : ', LogR_model.score(train_data, train_label))
-print('Logistic Regression Test Score : ', LogR_model.score(test_data, test_label))
-print('Logistic Regression Model Coefficient : ', LogR_model.coef_)
+print('Logistic Regression Train Score : ', LogR_model.score(train_data, train_label))	# Train 데이터에 대한 Classification 정확도 출력
+print('Logistic Regression Test Score : ', LogR_model.score(test_data, test_label))	# Test 데이터에 대한 Classification 정확도 출력
+print('Logistic Regression Model Coefficient : ', LogR_model.coef_)			# 학습된 Logistic Regression 모델의 Coefficient 출력
 
-LogR_acc = LogR_model.score(test_data, test_label)
+LogR_acc = LogR_model.score(test_data, test_label)	# Logistic Regression 모델의 정확도 저장
 
-### KNN ###
+### 유방암 데이터셋에 대해 KNN Classification 적용 ###
+# K의 값을 0에서 100까지 바꿔가면서 KNN Classification 수행함
 k_list = range(1, 101)
 accuracy = []
 
 for k in k_list:
+    # 0에서 100 사이의 K값을 바꿔가면서 유방암 데이터셋에 대한 Fitting / Classification 수행
     knn_model = KNeighborsClassifier(n_neighbors=k)
+    
+    # KNN Classifier에 대해 Fitting을 수행하여 K개 Neighbor 탐색에 용의한 자료 구조로 데이터를 배치함
     knn_model.fit(train_data, train_label)
 
+    # 0에서 100 사이의 K값을 바꿔가면서 Test 데이터에 대한 KNN의 Prediction 수행
     prediction = knn_model.predict(test_data)
 
+    # 0에서 100 사이의 K값을 바꿔가면서 KNN의 Classification 정확도를 저장함
     accuracy.append(metrics.accuracy_score(prediction, test_label))
 
-maxKNN_acc = max(accuracy)
+maxKNN_acc = max(accuracy)	# KNN이 가지는 최대 정확도 저장
 
-### Gaussian Naive Bayes ###
-GaussiaNB_model = GaussianNB()
-GaussiaNB_model.fit(train_data, train_label)
+### 유방암 데이터셋에 대해 Gaussian Naive Bayes 이용한 Classification 적용 ###
+GaussiaNB_model = GaussianNB()				# Gaussian Naive Bayes Classifier 준비
+GaussiaNB_model.fit(train_data, train_label)		# Train 데이터에 대해 Gaussian Naive Bayes Fitting 수행
 
-prediction = GaussiaNB_model.predict(test_data)
+prediction = GaussiaNB_model.predict(test_data)		# Test 데이터에 대해 Gaussian Naive Bayes Classifier의 Classification 수행
 
-GaussianNB_acc = metrics.accuracy_score(prediction, test_label)
+GaussianNB_acc = metrics.accuracy_score(prediction, test_label)	# Gaussian Naive Bayes 모델의 정확도 저장
 
-### Random Forest ###
-RandomForest = RandomForestClassifier(n_estimators=100)
-RandomForest.fit(train_data, train_label)
+### 유방암 데이터셋에 대해 Random Forest 이용한 Classification 적용 ###
+RandomForest = RandomForestClassifier(n_estimators=100)		# Random Forest Classifier 준비
+RandomForest.fit(train_data, train_label)			# Train 데이터에 대해 Random Forest Fitting 수행
 
-prediction = RandomForest.predict(test_data)
+prediction = RandomForest.predict(test_data)			# Test 데이터에 대해 Random Forest Classifier의 Classification 수행
 
-RandomForest_acc = metrics.accuracy_score(prediction, test_label)
+RandomForest_acc = metrics.accuracy_score(prediction, test_label)	# Random Forest Classifier의 정확도 저장
 
-### Decision Tree ###
-DecisionTree = DecisionTreeClassifier()
-DecisionTree.fit(train_data, train_label)
+### 유방암 데이터셋에 대해 Decision Tree 이용한 Classification 적용 ###
+DecisionTree = DecisionTreeClassifier()			# Decision Tree Classifier 준비
+DecisionTree.fit(train_data, train_label)		# Train 데이터에 대해 Decision Tree Fitting 수행
 
-prediction = DecisionTree.predict(test_data)
+prediction = DecisionTree.predict(test_data)		# Test 데이터에 대해 Decision Tree Classifier의 Classification 수행
 
-DecisionTree_acc = metrics.accuracy_score(prediction, test_label)
+DecisionTree_acc = metrics.accuracy_score(prediction, test_label)	# Decision Tree Classifier의 정확도 저장
 
-### Support Vector Machine ###
-SVM = svm.SVC(kernel='rbf', C=1, gamma=0.1)
-SVM.fit(train_data, train_label)
+### 유방암 데이터셋에 대해 Support Vector Machine 이용한 Classification 적용 ###
+SVM = svm.SVC(kernel='rbf', C=1, gamma=0.1)		# SVM Classifier 준비 / Non-linear Decision Boundary를 위한 kernel을 정의해줌
+SVM.fit(train_data, train_label)			# Train 데이터에 대해 Label간 Margin이 최대가 될 수 있도록 Fitting 수행
 
-prediction = SVM.predict(test_data)
+prediction = SVM.predict(test_data)			# Test 데이터에 대해 SVM으로 Classification 수행
 
-SVM_acc = metrics.accuracy_score(prediction, test_label)
+SVM_acc = metrics.accuracy_score(prediction, test_label)	# SVM Classifier의 정확도 저장
 
+
+
+### 각각의 Classifier의 정확도를 막대 그래프로 표현하고, 각 정확도 값을 막대 그래프 위에 표시함 ###
 print()
 print('-----[Prediction Results on Breast Cancer Dataset]-----')
 print('Prediction Accuracy of Logistic Accuracy : ', LogR_acc)
@@ -109,21 +127,27 @@ plt.figure(figsize=[20, 8])
 
 barlist = plt.bar(models, accuracy)
 
+# Logistic Regression Classifier의 정확도 막대 그래프
 barlist[0].set_color('red')
 plt.text(barlist[0].get_x(), accuracy[0] + 0.005, s=str(accuracy[0]), fontweight='bold')
 
+# KNN Classifier의 정확도 막대 그래프
 barlist[1].set_color('orange')
 plt.text(barlist[1].get_x(), accuracy[1] + 0.005, s=str(accuracy[1]), fontweight='bold')
 
+# Gaussian Naive Bayes Classifier의 정확도 막대 그래프
 barlist[2].set_color('blue')
 plt.text(barlist[2].get_x(), accuracy[2] + 0.005, s=str(accuracy[2]), fontweight='bold')
 
+# Random Forest Classifier의 정확도 막대 그래프
 barlist[3].set_color('green')
 plt.text(barlist[3].get_x(), accuracy[3] + 0.005, s=str(accuracy[3]), fontweight='bold')
 
+# Decision Tree Classifier의 정확도 막대 그래프
 barlist[4].set_color('purple')
 plt.text(barlist[4].get_x(), accuracy[4] + 0.005, s=str(accuracy[4]), fontweight='bold')
 
+# SVM Classifier의 정확도 막대 그래프
 barlist[5].set_color('yellow')
 plt.text(barlist[5].get_x(), accuracy[5] + 0.005, s=str(accuracy[5]), fontweight='bold')
 
--- Assignment_04_Classification/KOR_ver/04_Standardization_Normalization (KOR).py
+++ Assignment_04_Classification/KOR_ver/04_Standardization_Normalization (KOR).py
@@ -1,6 +1,6 @@
-#############################################################################
-### Breaset Cancer Classification using Standardization and Normalization ###
-#############################################################################
+#################################################################################
+### Q4. Breaset Cancer Classification using Standardization and Normalization ###
+#################################################################################
 
 import numpy as np
 import pandas as pd
@@ -19,142 +19,182 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
 
-breast_cancer_dataset = load_breast_cancer()
+### 데이터셋 준비 ###
+breast_cancer_dataset = load_breast_cancer()	# 유방암 데이터셋 준비
 
-print('Breast Cancer Dataset Key : ', breast_cancer_dataset.keys())
+print('Breast Cancer Dataset Key : ', breast_cancer_dataset.keys())		# 유방암 데이터셋의 Attribute (구성요소) 출력
 
-print('Breast Cancer Dataset Target : ', breast_cancer_dataset.target_names)
+print('Breast Cancer Dataset Target : ', breast_cancer_dataset.target_names)	# 유방암 데이터셋의 Target/Label에 대한 내용 출력 (유방암 악성 여부)
 
-breast_cancer_data = pd.DataFrame(breast_cancer_dataset.data)
-breast_cancer_label = pd.DataFrame(breast_cancer_dataset.target)
+breast_cancer_data = pd.DataFrame(breast_cancer_dataset.data)			# 유방암 데이터셋의 Feature 데이터를 Pandas DataFrame 형태로 전환
+breast_cancer_label = pd.DataFrame(breast_cancer_dataset.target)		# 유방암 데이터셋의 Target/Label을 Pandas DataFrame 형태로 전환
 
-print(breast_cancer_data.head())
+print(breast_cancer_data.head())	# 유방암 데이터셋의 Feature 데이터 최초 5개를 출력함
+					# 유방암 데이터셋의 30개의 Feature로 구성되어있다는 것을 알 수 있음
 
-print(breast_cancer_label.head())
+print(breast_cancer_label.head())	# 유방암 데이터셋의 Target/Label 데이터 최초 5개를 출력함
 
-print(breast_cancer_data.describe())
+print(breast_cancer_data.describe())	# 유방암 데이터셋의 Feature 데이터에 대한 통계학적 정보를 출력함
 
-# Standardization : Standardize the dataset into z-score
-mean = breast_cancer_dataset.data.mean(axis=0)
-standard_deviation = breast_cancer_dataset.data.std(axis=0)
+### 데이터셋 표준화 (Standardization) : Feature 데이터에 대해서 Mean과 Standard Deviation를 이용한 z-score로 표준화를 수행함 ###
+mean = breast_cancer_dataset.data.mean(axis=0)			# Feature 데이터의 Mean을 산출함
+standard_deviation = breast_cancer_dataset.data.std(axis=0)	# Feature 데이터의 Standard Deviation을 산출함
 
-standardized_breast_cancer_data = (breast_cancer_dataset.data - mean) / standard_deviation
+standardized_breast_cancer_data = (breast_cancer_dataset.data - mean) / standard_deviation	# (X-Mean) / Standard Deviation을 통해 z-score로 표준화함
 
-# Normalization : Normalizae the dataset as the value between 0 and 1 using min-max normalization
-dataset_min = breast_cancer_dataset.data.min(axis=0)
-dataset_max = breast_cancer_dataset.data.max(axis=0)
-normalized_breast_cancer_data = (breast_cancer_dataset.data - dataset_min) / (dataset_max - dataset_min)
+### 데이터셋 정규화 Normalization : Feature 데이터에 대해서 Min-Max Normalization을 통해 모든 데이터를 0 ~ 1 사이 범위로 재정의함 ###
+dataset_min = breast_cancer_dataset.data.min(axis=0)	# Feature 데이터의 최소값 산출
+dataset_max = breast_cancer_dataset.data.max(axis=0)	# Feature 데이터의 최대값 산
+normalized_breast_cancer_data = (breast_cancer_dataset.data - dataset_min) / (dataset_max - dataset_min)	# Min-Max Normalization으로 데이터셋 구성
 
-# Plotting comparison between original / standardized / normalized breast cancer dataset
+# 기존 Feature 데이터, 표준화된 Feature 데이터, 정규화된 Feature 데이터를 비교하기 위해 각각에 대해 Box Plot 작성
 main_fig = plt.figure(figsize=[20, 8])
-gs = main_fig.add_gridspec(2, 6)
+gs = main_fig.add_gridspec(2, 6)	# Plotting 영역을 세부적으로 제어하기 위해 gridspec 사용함
 
+# 기존 Feature 데이터에 대한 Box Plot 작성
 fg_ax1 = main_fig.add_subplot(gs[0, 0:2])
 fg_ax1.boxplot(breast_cancer_dataset.data)
 plt.xticks(np.arange(len(breast_cancer_dataset.feature_names))+1, breast_cancer_dataset.feature_names, rotation=90)
 fg_ax1.set_title('Original Breast Cancer Dataset\n(No Standardization or Normalization)', fontdict={'weight':'bold'})
 
+# 표준화된 Feature 데이터에 대한 Box Plot 작성
 fg_ax2 = main_fig.add_subplot(gs[0, 2:4])
 fg_ax2.boxplot(standardized_breast_cancer_data)
 plt.xticks(np.arange(len(breast_cancer_dataset.feature_names))+1, breast_cancer_dataset.feature_names, rotation=90)
 fg_ax2.set_title('Standardized Breast Cancer Dataset', fontdict={'weight':'bold'})
 
+# 정규화된 Feature 데이터에 대한 Box Plot 작성
 fg_ax3 = main_fig.add_subplot(gs[0, 4:6])
 fg_ax3.boxplot(normalized_breast_cancer_data)
 plt.xticks(np.arange(len(breast_cancer_dataset.feature_names))+1, breast_cancer_dataset.feature_names, rotation=90)
 fg_ax3.set_title('Normalized Breast Cancer Dataset', fontdict={'weight':'bold'})
 
-# Split original / standardized / normalized breast cancer dataset into 70% training set and 30% test set
-original_train_data, original_test_data, original_train_label, original_test_label = train_test_split(breast_cancer_data, breast_cancer_label, test_size=0.3, random_state=23)
+### 데이터셋 Training / Test 분리 ###
+# 기본 Feature 데이터, 표준화된 Feature 데이터, 정규환된 Feature 데이터를 각각 Training 70% : Test 30%로 분리함
+# 기존 Feature 데이터를 Training 70% : Test 30%로 분리함
+original_train_data, original_test_data, original_train_label, original_test_label = train_test_split(breast_cancer_data, 
+												      breast_cancer_label, 
+												      test_size=0.3, 
+												      random_state=23)
+# 표준화된 Feature 데이터를 Training 70% : Test 30%로 분리함
+standardized_train_data, standardized_test_data, standardized_train_label, standardized_test_label= train_test_split(standardized_breast_cancer_data, 
+														     breast_cancer_label, 
+														     test_size=0.3, 
+														     random_state=23)
+# 정규화된 Feature 데이터를 Training 70% : Test 30%로 분리함
+normalized_train_data, normalized_test_data, normalized_train_label, normalized_test_label = train_test_split(normalized_breast_cancer_data, 
+													      breast_cancer_label, 
+													      test_size=0.3, 
+													      random_state=23)
 
-standardized_train_data, standardized_test_data, standardized_train_label, standardized_test_label = train_test_split(standardized_breast_cancer_data, breast_cancer_label, test_size=0.3, random_state=23)
+# 각 데이터셋 종류별로 Train, Test를 정리하여 Dictionary 형태로 저장하고, 각각의 Classification에 적용함
+dataset_group = [{'name' : 'original', 'train_data' : original_train_data, 'train_label' : original_train_label, 
+		  'test_data' : original_test_data, 'test_label' : original_test_label},
+                 {'name' : 'standardized', 'train_data' : standardized_train_data, 'train_label' : standardized_train_label, 
+		  'test_data' : standardized_test_data, 'test_label' : standardized_test_label},
+                 {'name' : 'normalized', 'train_data' : normalized_train_data, 'train_label' : normalized_train_label, 
+		  'test_data' : normalized_test_data, 'test_label' : normalized_test_label}]
 
-normalized_train_data, normalized_test_data, normalized_train_label, normalized_test_label = train_test_split(normalized_breast_cancer_data, breast_cancer_label, test_size=0.3, random_state=23)
+# 각 데이터셋 종류별로 적용한 Classification의 정확도를 각각 Dictionary 형태로 저장함
+accuracy = [{'name' : 'original', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 
+	     'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None},
+            {'name' : 'standardized', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 
+	     'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None},
+            {'name' : 'normalized', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 
+	     'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None}]
 
-# Prepare dictionary for each dataset and accuracy for iterating the datasets with various classification methods
-dataset_group = [{'name' : 'original', 'train_data' : original_train_data, 'train_label' : original_train_label, 'test_data' : original_test_data, 'test_label' : original_test_label},
-                 {'name' : 'standardized', 'train_data' : standardized_train_data, 'train_label' : standardized_train_label, 'test_data' : standardized_test_data, 'test_label' : standardized_test_label},
-                 {'name' : 'normalized', 'train_data' : normalized_train_data, 'train_label' : normalized_train_label, 'test_data' : normalized_test_data, 'test_label' : normalized_test_label}]
-
-accuracy = [{'name' : 'original', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None},
-            {'name' : 'standardized', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None},
-            {'name' : 'normalized', 'LogR_acc' : None, 'maxKNN_acc' : None, 'GaussianNB_acc' : None, 'RandomForest_acc' : None, 'DecisionTree_acc' : None, 'SVM_acc' : None}]
-
+### 각 데이터셋 종류별로 Logistic Regression, KNN, Gaussian Naive Bayes, Random Forest, Decision Tree, SVM을 적용함 ###
 for dataset in dataset_group:
 
-    ### Logistic Regression ###
-    LogR_model = LogisticRegression()
-    LogR_model.fit(dataset['train_data'], dataset['train_label'])
+    ### Logistic Regression 이용한 Classification 수행 ###
+    LogR_model = LogisticRegression()					# Logistic Regression Classifier 준비
+    LogR_model.fit(dataset['train_data'], dataset['train_label'])	# Train 데이터에 대해 Logistic Regression Fitting 수행
 
     for acc in accuracy:
         if acc['name'] == dataset['name']:
-            acc['LogR_acc'] = LogR_model.score(dataset['test_data'], dataset['test_label'])
+            acc['LogR_acc'] = LogR_model.score(dataset['test_data'], dataset['test_label'])	# Logistic Regression 모델의 정확도 저장
 
-    ### KNN ###
+    ### KNN 이용한 Classification 수행 ###
+    # K의 값을 0에서 100까지 바꿔가면서 KNN Classification 수행함
     k_list = range(1, 101)
     knn_accuracy = []
 
     for k in k_list:
+        # 0에서 100 사이의 K값을 바꿔가면서 유방암 데이터셋에 대한 Fitting / Classification 수행
         knn_model = KNeighborsClassifier(n_neighbors=k)
+
+        # KNN Classifier에 대해 Fitting을 수행하여 K개 Neighbor 탐색에 용의한 자료 구조로 데이터를 배치함
         knn_model.fit(dataset['train_data'], dataset['train_label'])
 
+        # 0에서 100 사이의 K값을 바꿔가면서 Test 데이터에 대한 KNN의 Prediction 수행
         prediction = knn_model.predict(dataset['test_data'])
 
+        # 0에서 100 사이의 K값을 바꿔가면서 KNN의 Classification 정확도를 저장함
         knn_accuracy.append(metrics.accuracy_score(prediction, dataset['test_label']))
 
     for acc in accuracy:
         if acc['name'] == dataset['name']:
-            acc['maxKNN_acc'] = max(knn_accuracy)
+            acc['maxKNN_acc'] = max(knn_accuracy)	# KNN이 가지는 최대 정확도 저장
 
-    ### Gaussian Naive Bayes ###
-    GaussiaNB_model = GaussianNB()
-    GaussiaNB_model.fit(dataset['train_data'], dataset['train_label'])
+    ### Gaussian Naive Bayes 이용한 Classification 수행 ###
+    GaussiaNB_model = GaussianNB()						# Gaussian Naive Bayes Classifier 준비
+    GaussiaNB_model.fit(dataset['train_data'], dataset['train_label'])		# Train 데이터에 대해 Gaussian Naive Bayes Fitting 수행
 
-    prediction = GaussiaNB_model.predict(dataset['test_data'])
-
-    for acc in accuracy:
-        if acc['name'] == dataset['name']:
-            acc['GaussianNB_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])
-
-    ### Random Forest ###
-    RandomForest = RandomForestClassifier(n_estimators=100)
-    RandomForest.fit(dataset['train_data'], dataset['train_label'])
-
-    prediction = RandomForest.predict(dataset['test_data'])
+    prediction = GaussiaNB_model.predict(dataset['test_data'])			# Test 데이터에 대해 Gaussian Naive Bayes Classifier의 Classification 수행
 
     for acc in accuracy:
         if acc['name'] == dataset['name']:
-            acc['RandomForest_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])
+            acc['GaussianNB_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])	# Gaussian Naive Bayes 모델의 정확도 저장
 
-    ### Decision Tree ###
-    DecisionTree = DecisionTreeClassifier()
-    DecisionTree.fit(dataset['train_data'], dataset['train_label'])
+    ### Random Forest 이용한 Classification 수행 ###
+    RandomForest = RandomForestClassifier(n_estimators=100)			# Random Forest Classifier 준비
+    RandomForest.fit(dataset['train_data'], dataset['train_label'])		# Train 데이터에 대해 Random Forest Fitting 수행
 
-    prediction = DecisionTree.predict(dataset['test_data'])
-
-    for acc in accuracy:
-        if acc['name'] == dataset['name']:
-            acc['DecisionTree_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])
-
-    ### Support Vector Machine ###
-    SVM = svm.SVC(kernel='rbf', C=1, gamma=0.1)
-    SVM.fit(dataset['train_data'], dataset['train_label'])
-
-    prediction = SVM.predict(dataset['test_data'])
+    prediction = RandomForest.predict(dataset['test_data'])			# Test 데이터에 대해 Random Forest Classifier의 Classification 수행
 
     for acc in accuracy:
         if acc['name'] == dataset['name']:
-            acc['SVM_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])
+            acc['RandomForest_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])	# Random Forest Classifier의 정확도 저장
 
-# Plot various classification accuracy for each type of datasets
+    ### Decision Tree 이용한 Classification 수행 ###
+    DecisionTree = DecisionTreeClassifier()					# Decision Tree Classifier 준비
+    DecisionTree.fit(dataset['train_data'], dataset['train_label'])		# Train 데이터에 대해 Decision Tree Fitting 수행
+
+    prediction = DecisionTree.predict(dataset['test_data'])			# Test 데이터에 대해 Decision Tree Classifier의 Classification 수행
+
+    for acc in accuracy:
+        if acc['name'] == dataset['name']:
+            acc['DecisionTree_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])	# Decision Tree Classifier의 정확도 저장
+
+    ### Support Vector Machine 이용한 Classification 수행 ###
+    SVM = svm.SVC(kernel='rbf', C=1, gamma=0.1)					# SVM Classifier 준비
+    SVM.fit(dataset['train_data'], dataset['train_label'])			# Train 데이터에 대해 Label간 Margin이 최대가 될 수 있도록 Fitting 수행
+
+    prediction = SVM.predict(dataset['test_data'])				# Test 데이터에 대해 SVM으로 Classification 수행
+
+    for acc in accuracy:
+        if acc['name'] == dataset['name']:
+            acc['SVM_acc'] = metrics.accuracy_score(prediction, dataset['test_label'])		# SVM Classifier의 정확도 저장
+
+# 표준화와 정규화 적용 여부가 각각의 Classification 정확도에 어떤 영향을 주는지 비교하기 위해 각 데이터셋 별로 Classifier의 정확도를 Bar Plot으로 작성함
 models = ['Logistic\nRegression', 'KNN', 'GaussianNB', 'Random\nForest', 'Decision\nTree', 'SVM']
 
 width = 0.35
 fg_ax4 = main_fig.add_subplot(gs[1, 0:6])
-fg_ax4.bar(np.arange(len(models)) - width/3, [accuracy[0]['LogR_acc'], accuracy[0]['maxKNN_acc'], accuracy[0]['GaussianNB_acc'], accuracy[0]['RandomForest_acc'], accuracy[0]['DecisionTree_acc'], accuracy[0]['SVM_acc']], width=width/3, label='Original Dataset')
-fg_ax4.bar(np.arange(len(models)), [accuracy[1]['LogR_acc'], accuracy[1]['maxKNN_acc'], accuracy[1]['GaussianNB_acc'], accuracy[1]['RandomForest_acc'], accuracy[1]['DecisionTree_acc'], accuracy[1]['SVM_acc']], width=width/3, label='Standardized Dataset')
-fg_ax4.bar(np.arange(len(models)) + width/3, [accuracy[2]['LogR_acc'], accuracy[2]['maxKNN_acc'], accuracy[2]['GaussianNB_acc'], accuracy[2]['RandomForest_acc'], accuracy[2]['DecisionTree_acc'], accuracy[2]['SVM_acc']], width=width/3, label='Normalized Dataset')
+fg_ax4.bar(np.arange(len(models)) - width/3, [accuracy[0]['LogR_acc'], accuracy[0]['maxKNN_acc'], 
+	   accuracy[0]['GaussianNB_acc'], accuracy[0]['RandomForest_acc'], accuracy[0]['DecisionTree_acc'], 
+	   accuracy[0]['SVM_acc']], width=width/3, label='Original Dataset')
+
+fg_ax4.bar(np.arange(len(models)), [accuracy[1]['LogR_acc'], accuracy[1]['maxKNN_acc'], 
+	   accuracy[1]['GaussianNB_acc'], accuracy[1]['RandomForest_acc'], accuracy[1]['DecisionTree_acc'], 
+	   accuracy[1]['SVM_acc']], width=width/3, label='Standardized Dataset')
+
+fg_ax4.bar(np.arange(len(models)) + width/3, [accuracy[2]['LogR_acc'], accuracy[2]['maxKNN_acc'], 
+	   accuracy[2]['GaussianNB_acc'], accuracy[2]['RandomForest_acc'], accuracy[2]['DecisionTree_acc'], 
+	   accuracy[2]['SVM_acc']], width=width/3, label='Normalized Dataset')
+
 fg_ax4.set_title('Classification Result Comparison : Original / Standardized / Normalized', fontdict={'weight':'bold', 'size' : 15})
+
 plt.xticks(np.arange(len(models)), models, fontweight='bold')
 plt.xlim(-0.3, 6.1)
 plt.ylim(0, 1.1)
