cc4c968faab4fcd9f01402693fd8bb77c2118af6
luwis93choi <luwis93choi@hotmail.com>
2020-11-07 18:05:42 +0900

    Merged PCA and IPCA examples / Renamed examples

--- Assignment_07_Dimensionality_Reduction_PCA/01_4_PCA_IPCA_MNIST_Compression.py
+++ Assignment_07_Dimensionality_Reduction_PCA/01_4_PCA_IPCA_MNIST_Compression.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
 
 # Define the directory for saving the plotting results
 PROJECT_ROOT_DIR = '.'
@@ -22,6 +23,22 @@ def save_fig(fig_id, tight_layout=True, fig_extension='png', resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
+# MNIST dataset display function
+def plot_digits(instances, images_per_row=5, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
+    
 ####################################################################################################################################################
 ### Prepare MNIST dataset ##########################################################################################################################
 ####################################################################################################################################################
@@ -34,7 +51,7 @@ y = mnist['target']     # Load target values from MNIST dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y)   # Split the dataset between training and test
 
 ####################################################################################################################################################
-### Principle Component Analysis ###################################################################################################################
+### PCA (Principle Component Analysis) #############################################################################################################
 ####################################################################################################################################################
 pca = PCA()             # PCA object for producing principle components for the dataset
 pca.fit(X_train)        # Produce Re-Organized dataset (sklearn API includes standardization)
@@ -69,25 +86,7 @@ pca = PCA(n_components=154)                     # PCA object for producing 154 p
 X_reduced = pca.fit_transform(X_train)          # Produce Re-Organized dataset (sklearn API includes standardization)
 X_recovered = pca.inverse_transform(X_reduced)  # Reproject Re-Organized dataset back to original feature space
 
-# MNIST dataset display function
-def plot_digits(instances, images_per_row=5, **options):
-    size = 28
-    images_per_row = min(len(instances), images_per_row)
-    images = [instance.reshape(size,size) for instance in instances]
-    n_rows = (len(instances) - 1) // images_per_row + 1
-    row_images = []
-    n_empty = n_rows * images_per_row - len(instances)
-    images.append(np.zeros((size, size * n_empty)))
-    for row in range(n_rows):
-        rimages = images[row * images_per_row : (row + 1) * images_per_row]
-        row_images.append(np.concatenate(rimages, axis=1))
-    image = np.concatenate(row_images, axis=0)
-    plt.imshow(image, cmap = mpl.cm.binary, **options)
-    plt.axis("off")
-
-####################################################################################################################################################
-### Comparison between original image dataset and PCA image dataset ################################################################################
-####################################################################################################################################################
+# Comparison between original image dataset and PCA image dataset 
 plt.figure(figsize=(7, 4))
 plt.subplot(121)
 plot_digits(X_train[::2100])                # Show training dataset of MNIST
@@ -97,4 +96,28 @@ plot_digits(X_recovered[::2100])            # Show PCA-reprojected training data
 plt.title("Compressed", fontsize=16)
 
 save_fig("mnist_compression_plot")
+plt.show()
+
+####################################################################################################################################################
+### IPCA (Incremental Principle Component Analysis) ################################################################################################
+####################################################################################################################################################
+n_batches = 100
+
+inc_pca = IncrementalPCA(n_components=154)
+
+for X_batch in np.array_split(X_train, n_batches):
+    print('.', end='')
+    inc_pca.partial_fit(X_batch)
+
+X_reduced = inc_pca.transform(X_train)
+
+X_recovered_inc_pca = inc_pca.inverse_transform(X_reduced)
+
+# Comparison between PCA image dataset and IPCA image dataset 
+plt.figure(figsize=(7, 4))
+plt.subplot(121)
+plot_digits(X_train[::2100])
+plt.subplot(122)
+plot_digits(X_recovered_inc_pca[::2100])
+plt.tight_layout()
 plt.show()
